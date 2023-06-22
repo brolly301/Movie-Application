b3e09f8e0de214fa23d79f16164c4016407723aa
brolly301 <marcrobertjohn@gmail.com>
2023-06-22 12:36:48 +0100

    Update movieList, filtering correctly but duplicating due to duplicate times

--- client/src/components/ShowtimesPage/MovieList.js
+++ client/src/components/ShowtimesPage/MovieList.js
@@ -4,13 +4,19 @@ import MovieShow from "./MovieShow";
 export default function MovieList() {
   const { movies, date } = useMovieContext();
 
-  const renderedList = movies?.map((movie) => {
-    movie.shows.filter((show) => {
-      if (show.date === date) {
-        return <MovieShow key={movie.imdbID} movie={movie} link="showtimes" />;
-      }
-    });
-  });
+  const renderedList = movies?.map((movie) => movie.shows.map((show)=>{
+    if (show.date === date) {
+            return <MovieShow key={movie.imdbID} movie={movie} link="showtimes" />;
+          }
+  }))
+
+  // const renderedList = movies?.map((movie) => {
+  //   movie.shows.filter((show) => {
+  //     if (show.date === date) {
+  //       return <MovieShow key={movie.imdbID} movie={movie} link="showtimes" />;
+  //     }
+  //   });
+  // });
 
   return <div>{renderedList}</div>;
 }
