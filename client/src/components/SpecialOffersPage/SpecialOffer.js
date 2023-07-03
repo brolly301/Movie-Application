009a33ec84eef4237b5849c32f19779883b750d7
Marc Brolly <marcrobertjohn@gmail.com>
2023-07-03 20:30:59 +0100

    Update CSS for special offers page

--- client/src/components/SpecialOffersPage/SpecialOffer.js
+++ client/src/components/SpecialOffersPage/SpecialOffer.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "../../CSS/SpecialOffers/SpecialOffers.css";
 
-const SpecialOffer = () => {
+const SpecialOffer = ({ children, details, image }) => {
   return (
     <div className="special-offers-container">
-      <div className="special-offers-container">
-        <h3>Manic Mondays</h3>
-        <p>This is a test example of this special offer</p>
-        <img src="https://plus.unsplash.com/premium_photo-1675629311338-edc84c9b7c79?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" />
+      <h2>{children}</h2>
+      <div className="special-offers-image-container">
+        <img src={image} />
       </div>
+      <p>{details}</p>
     </div>
   );
 };
--- client/src/pages/SpecialOffersPage.js
+++ client/src/pages/SpecialOffersPage.js
@@ -1,8 +1,13 @@
 import SpecialOffersList from "../components/SpecialOffersPage/SpecialOffersList";
+import "../CSS/SpecialOffers/SpecialOffers.css";
+
 export default function SpecialOffersPage() {
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>Special Offers</h1>
+    <div className="special-offers-page-container">
+      <h1>Special Offers</h1>
+      <h2 className="special-offers-heading">
+        Explore Our Current special offers and deals
+      </h2>
       <SpecialOffersList />
     </div>
   );
