2c8ffb41b7fa32ea79e28798bd79063f003e4275
brolly301 <marcrobertjohn@gmail.com>
2023-07-13 20:11:30 +0100

    Change footer styling to suit the rest of the page, need to fix the HR div as its pushing the footer back up due to it no longer being the roots child element

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,50 +7,54 @@ export default function Footer() {
   const { userData } = useUserContext();
 
   return (
-    <div className="footer-container">
-      <div>
-        <h3>Socials</h3>
-        <h4>Twitter</h4>
-        <h4>Instagram</h4>
-        <h4>Facebook</h4>
-      </div>
-      <div>
-        <h3>Account</h3>
-        <Link
-          className="footer-link"
-          to={userData.user ? "/account" : "/login"}
-        >
-          <h4>Profile</h4>
-        </Link>
-        <Link
-          className="footer-link"
-          to={userData.user ? "/account" : "/login"}
-        >
-          <h4>Bookings</h4>
-        </Link>
-        <Link
-          className="footer-link"
-          to={userData.user ? "/account" : "/login"}
-        >
-          <h4>Loyalty</h4>
-        </Link>
-      </div>
-      <div>
-        <h3>Explore</h3>
-        <Link className="footer-link" to={"/showtimes"}>
-          <h4>Book Movie</h4>
-        </Link>
-        <Link className="footer-link" to={"/specialOffers"}>
-          <h4>Browse Offers</h4>
-        </Link>
-        <Link className="footer-link" to={"/contactUs"}>
-          <h4>Contact Us</h4>
-        </Link>
-      </div>
-      <div className="footer-subscribe">
-        <h3>Find Out Latest Deals</h3>
-        <input type="text" placeholder="Enter email address..." />
-        <button>Subscribe</button>
+    <div>
+      <hr></hr>
+
+      <div className="footer-container">
+        <div>
+          <h3>Socials</h3>
+          <h4>Twitter</h4>
+          <h4>Instagram</h4>
+          <h4>Facebook</h4>
+        </div>
+        <div>
+          <h3>Account</h3>
+          <Link
+            className="footer-link"
+            to={userData.user ? "/account" : "/login"}
+          >
+            <h4>Profile</h4>
+          </Link>
+          <Link
+            className="footer-link"
+            to={userData.user ? "/account" : "/login"}
+          >
+            <h4>Bookings</h4>
+          </Link>
+          <Link
+            className="footer-link"
+            to={userData.user ? "/account" : "/login"}
+          >
+            <h4>Loyalty</h4>
+          </Link>
+        </div>
+        <div>
+          <h3>Explore</h3>
+          <Link className="footer-link" to={"/showtimes"}>
+            <h4>Book Movie</h4>
+          </Link>
+          <Link className="footer-link" to={"/specialOffers"}>
+            <h4>Browse Offers</h4>
+          </Link>
+          <Link className="footer-link" to={"/contactUs"}>
+            <h4>Contact Us</h4>
+          </Link>
+        </div>
+        <div className="footer-subscribe">
+          <h3>Find Out Latest Deals</h3>
+          <input type="text" placeholder="Enter email address..." />
+          <button>Subscribe</button>
+        </div>
       </div>
     </div>
   );
