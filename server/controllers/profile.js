d3880c8de637c7636557bddbce2b2d8561768e8c
brolly301 <marcrobertjohn@gmail.com>
2023-07-14 22:53:17 +0100

    Add styling to auth list

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -13,6 +13,18 @@ exports.editProfileDetails = async (req, res) => {
   }
 };
 
+exports.editLoginDetails = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.user._id, { ...req.body });
+    res.status(200).json({
+      message: "Edit successful",
+      user,
+    });
+  } catch (err) {
+    res.send(err);
+  }
+};
+
 exports.getBookingDetails = async (req, res) => {
   const bookings = await Booking.find({ email: req.user.email }).populate(
     "movieID"
