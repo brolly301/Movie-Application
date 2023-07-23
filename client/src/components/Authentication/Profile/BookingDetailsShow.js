f240efda75622fb1a8d7661439e2476a4e29ed37
brolly301 <marcrobertjohn@gmail.com>
2023-07-23 20:53:51 +0100

    Add responsive design to booking details profile component

--- client/src/components/Authentication/Profile/BookingDetailsShow.js
+++ client/src/components/Authentication/Profile/BookingDetailsShow.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { deleteBooking } from "../../../APIs/profile";
 import "../../../CSS/Authentication/BookingDetails.css";
 import { toast } from "react-toastify";
 
 export default function BookingDetailsShow({ booking }) {
+  const [active, setActive] = useState(false);
+
   const handleClick = () => {
     deleteBooking(booking._id);
     toast.success("Booking successfully cancelled.");
@@ -12,37 +15,54 @@ export default function BookingDetailsShow({ booking }) {
 
   return (
     <div className="booking-details-container">
-      <img src={booking.movieID.poster} alt="" />
-
-      <div className="booking-details-times-container">
-        <h3>
-          {booking.movieID.title}{" "}
+      <img
+        className="booking-details-image"
+        src={booking.movieID.poster}
+        alt=""
+      />
+      <div className="booking-details-hidden">
+        <h3 className="booking-details-title-hidden">
+          {booking.movieID.title}
           <img
             className="booking-title-icon"
             src={booking.movieID.rated}
             alt=""
           />
         </h3>
-        <div className="booking-details-date-time">
-          <div className="booking-details-date">
-            <h4>Date:</h4>
-            <h4>{booking.startDate.substring(0, 15)}</h4>
-          </div>
-          <div className="booking-details-time">
-            <h4>Time:</h4>
-            <h4>{booking.startTime}</h4>
-          </div>
-          <div className="booking-details-time">
-            <h4>Seats:</h4>
-            <h4>{booking.seatNumber?.map((seat) => `${seat} `)}</h4>
-          </div>
-        </div>
-        <button onClick={handleClick} className="booking-cancel-button">
-          Cancel
-        </button>
-      </div>
+        <img
+          className="booking-details-image-hidden"
+          src={booking.movieID.poster}
+          alt=""
+        />
 
-      <div className="movie-showtimes-container"></div>
+        <div className="booking-details-times-container">
+          <h3 className="booking-details-title">
+            {booking.movieID.title}
+            <img
+              className="booking-title-icon"
+              src={booking.movieID.rated}
+              alt=""
+            />
+          </h3>
+          <div className="booking-details-date-time">
+            <div className="booking-details-date">
+              <h4>Date:</h4>
+              <h4>{booking.startDate.substring(0, 15)}</h4>
+            </div>
+            <div className="booking-details-time">
+              <h4>Time:</h4>
+              <h4>{booking.startTime}</h4>
+            </div>
+            <div className="booking-details-time">
+              <h4>Seats:</h4>
+              <h4>{booking.seatNumber?.map((seat) => `${seat} `)}</h4>
+            </div>
+          </div>
+          <button onClick={handleClick} className="booking-cancel-button">
+            Cancel
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
