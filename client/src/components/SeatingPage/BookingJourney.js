34f04bf3a1c3b9bf77867b2b9cb226e757d7403b
brolly301 <marcrobertjohn@gmail.com>
2023-08-05 04:08:01 +0100

    Add responsive styling to booking journey

--- client/src/components/SeatingPage/BookingJourney.js
+++ client/src/components/SeatingPage/BookingJourney.js
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
 import { GoChevronRight } from "react-icons/go";
 import "../../CSS/Seating/BookingJourney.css";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 export default function BookingJourney() {
+  const size = useWindowSize();
   return (
     <div className="booking-journey-container">
       <ul className="booking-journey-ul">
         <li
           className={
             document.URL.includes("seating") ? "booking-journey-active" : ""
-          }>
-          Choose Seats <GoChevronRight className="booking-journey-icon" />
+          }
+        >
+          {size.width < 450 ? "Seats" : "Choose Seats"}
+          <GoChevronRight className="booking-journey-icon" />
         </li>
         <li
           className={
             document.URL.includes("tickets") ? "booking-journey-active" : ""
-          }>
-          Choose Tickets & Extras
+          }
+        >
+          {size.width < 450 ? "Tickets & Extras" : "Choose Tickets & Extras"}
           <GoChevronRight className="booking-journey-icon" />
         </li>
         <li
@@ -24,7 +29,8 @@ export default function BookingJourney() {
             document.URL.includes("confirmation")
               ? "booking-journey-active"
               : ""
-          }>
+          }
+        >
           Confirmation
         </li>
       </ul>
