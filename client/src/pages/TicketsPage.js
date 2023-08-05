1491a8fbd1d4bae4b7c41242cd8c3502a64f0744
brolly301 <marcrobertjohn@gmail.com>
2023-08-05 03:00:39 +0100

    Add most of the responsive styling for the tickets page

--- client/src/pages/TicketsPage.js
+++ client/src/pages/TicketsPage.js
@@ -6,12 +6,15 @@ import TicketSelection from "../components/SeatingPage/Tickets/TicketSelection";
 import useTicketContext from "../hooks/useTicketContext";
 import ExtraSelection from "../components/SeatingPage/Extras/ExtraSelection";
 import useExtraContext from "../hooks/useExtraContext";
+import { useWindowSize } from "@uidotdev/usehooks";
 
 export default function TicketsPage() {
   const location = useLocation();
   const movie = location.state.movie;
   const show = location.state.show;
   const seats = location.state.seats;
+
+  const size = useWindowSize();
 
   const { ticketData } = useTicketContext();
   const { extraData } = useExtraContext();
@@ -25,14 +28,29 @@ export default function TicketsPage() {
         <div className="seat-tickets-extras">
           <TicketSelection seats={seats} />
           <ExtraSelection seats={seats} />
+          {size.width <= 900 ? (
+            <SeatDetails
+              tickets={ticketData}
+              extras={extraData}
+              seats={seats}
+              movie={movie}
+              show={show}
+            />
+          ) : (
+            " "
+          )}
         </div>
-        <SeatDetails
-          tickets={ticketData}
-          extras={extraData}
-          seats={seats}
-          movie={movie}
-          show={show}
-        />
+        {size.width >= 900 ? (
+          <SeatDetails
+            tickets={ticketData}
+            extras={extraData}
+            seats={seats}
+            movie={movie}
+            show={show}
+          />
+        ) : (
+          " "
+        )}
       </div>
     </div>
   );
