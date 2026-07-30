ce42e99e73504156f5c6e82f8bdf9f31c1d7fe2b
Marc Brolly <marcrobertjohn@gmail.com>
2026-07-30 05:51:39 +0100

    Fix styling and responsiveness  of movie show

--- client/src/components/MoviePage/MovieTimes.js
+++ client/src/components/MoviePage/MovieTimes.js
@@ -2,16 +2,13 @@ import { Link } from "react-router-dom";
 import "../../CSS/Movies/MovieTimes.css";
 
 export default function MovieTimes({ movie, show }) {
-  console.log(show);
-
   return (
-    <div>
-      <Link
-        to={`/showtimes/${movie._id}/seating`}
-        state={{ movie: movie, show: show }}
-      >
-        <button className="movie-times-button">{show.startTime}</button>
-      </Link>
-    </div>
+    <Link
+      className="movie-times-button"
+      to={`/showtimes/${movie._id}/seating`}
+      state={{ movie, show }}
+    >
+      {show.startTime}
+    </Link>
   );
 }
--- client/src/components/ShowtimesPage/MovieShow.js
+++ client/src/components/ShowtimesPage/MovieShow.js
@@ -2,77 +2,65 @@ import "../../CSS/Movies/MovieShow.css";
 import { Link } from "react-router-dom";
 import MovieTimes from "../MoviePage/MovieTimes";
 import useMovieContext from "../../hooks/useMovieContext";
-import MovieShowShowtimes from "./MovieShowShowtimes";
 
 export default function MovieShow({ movie, link }) {
   const { date } = useMovieContext();
 
   const showsForDate = movie.shows.filter((show) => show.date === date);
-
-  const renderedList = showsForDate.map((show) => (
-    <MovieTimes key={show._id} show={show} movie={movie} />
-  ));
-
-  const splitActors = () =>
-    movie.actors.split(",").map((value) => {
-      return value.trim();
-    });
-  const splitGenres = () =>
-    movie.genre.split(",").map((value) => {
-      return value.trim();
-    });
-  const splitDirectors = () =>
-    movie.director.split(",").map((value) => {
-      return value.trim();
-    });
+  const firstActor = movie.actors.split(",")[0].trim();
+  const firstGenre = movie.genre.split(",")[0].trim();
+  const firstDirector = movie.director.split(",")[0].trim();
 
   return (
-    <div className="movie-container">
-      <div className="movie-details-hidden">
-        <h1 className="movie-show-title-hidden">
-          {movie.title}
-          <img src={movie.rated} className="movie-rating-icon" />
-        </h1>
-        <img className="movie-poster-hidden" src={movie.poster} alt="" />
-      </div>
-      <div className="movie-poster-details">
-        <img className="movie-poster" src={movie.poster} alt="" />
-        <div className="movie-details">
-          <h1>
-            {movie.title}
-            <img src={movie.rated} className="movie-rating-icon" />
-          </h1>
-          <div className="movie-show-details-container ">
-            <div className="movie-show-details-column-1">
-              <label>Running Time:</label>
-              <h3>{movie.runtime}</h3>
-              <label>Starring:</label>
-              <h3>{splitActors()[0]}</h3>
-            </div>
-            <div className="movie-show-details-column-2">
-              <label>Genre:</label>
-              <h3>{splitGenres()[0]}</h3>
-              <label>Director:</label>
-              <h3>{splitDirectors()[0]}</h3>
-            </div>
+    <article className="movie-container">
+      <img
+        className="movie-poster"
+        src={movie.poster}
+        alt={`${movie.title} poster`}
+      />
+      <div className="movie-details">
+        <header className="movie-title">
+          <h2>{movie.title}</h2>
+          <img
+            className="movie-rating-icon"
+            src={movie.rated}
+            alt={`${movie.title} age rating`}
+          />
+        </header>
+        <dl className="movie-meta">
+          <div>
+            <dt>Running time</dt>
+            <dd>{movie.runtime}</dd>
           </div>
-
-          <Link
-            className="movie-show-button"
-            state={{ movie }}
-            to={`/${link}/${movie._id}`}
-          >
-            More details
-          </Link>
+          <div>
+            <dt>Genre</dt>
+            <dd>{firstGenre}</dd>
+          </div>
+          <div>
+            <dt>Starring</dt>
+            <dd>{firstActor}</dd>
+          </div>
+          <div>
+            <dt>Director</dt>
+            <dd>{firstDirector}</dd>
+          </div>
+        </dl>
+        <Link
+          className="movie-show-button"
+          state={{ movie }}
+          to={`/${link}/${movie._id}`}
+        >
+          More details
+        </Link>
+      </div>
+      <section className="movie-showtimes-container">
+        <h2 className="showtimes-title">Showtimes</h2>
+        <div className="movie-showtimes">
+          {showsForDate.map((show) => (
+            <MovieTimes key={show._id} show={show} movie={movie} />
+          ))}
         </div>
-      </div>
-      <div className="movie-showtimes-container">
-        <MovieShowShowtimes
-          showtimes={renderedList}
-          movie={movie}
-          link={link}
-        />
-      </div>
-    </div>
+      </section>
+    </article>
   );
 }
